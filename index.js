afc7da020fe693e8b00e17a63e2558bfd56e3bdc
t3titi00 <t3titi00@students.oamk.fi>
2025-04-05 17:03:00 +0300

    V2

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response)=>{
-    response.send('Hello World!');
+    response.send('Hei Maailma!');
 });
 
 app.listen(PORT, () => {
-    console.log('Palvelin päällä portissa ${PORT}');
+    console.log(`Palvelin päällä portissa ${PORT}`);
 });
